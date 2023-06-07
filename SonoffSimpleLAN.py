01a90d0d7bea6f79ace2873ffaf912a08f6d8890
David Lee <4148025+dauheeIRL@users.noreply.github.com>
2023-06-07 18:52:12 +0100

    Update SonoffSimpleLAN.py
    can now watch for changes also

--- SonoffSimpleLAN.py
+++ SonoffSimpleLAN.py
@@ -2,10 +2,11 @@ import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 import collections
-import pysonofflanr3
+import sonoffcrypto
 import json
 import traceback
-
+from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
+import time
 
 def create_http_session():
 
@@ -35,8 +36,8 @@ def set_retries(http_session):
 
     # no retries at moment, control in sonoffdevice
     retries = Retry(
-        total=5,
-        backoff_factor=0.5,
+        total=8,
+        backoff_factor=0.8,
         method_whitelist=["POST"],
         status_forcelist=None,
     )
@@ -55,7 +56,7 @@ def get_update_payload(api_key, device_id: str, params: dict):
         "deviceid": device_id,
     }
 
-    pysonofflanr3.sonoffcrypto.format_encryption_msg(payload, api_key, params)
+    sonoffcrypto.format_encryption_msg(payload, api_key, params)
 
     return payload
 
@@ -74,7 +75,7 @@ def change_switch(api_key, device_id, ip_address, outlet, on_request):
         http_session = set_retries(http_session)
 
         if outlet == None:
-            #no outlet so we assume not strip device
+            #no outlet so we not strip device
             response = send(http_session, get_update_payload(api_key, device_id, {"switch": on_request, "outlet": int(0)}), 'http://' + ip_address + ":8081/zeroconf/switch")
         else:
             params = {"switches": [{"switch": "x", "outlet": 0}]}
@@ -90,13 +91,86 @@ def change_switch(api_key, device_id, ip_address, outlet, on_request):
         return strReturn
 
     except:
-        return traceback.format_exc()
+        return 'change_switch error setting device %s to state %s : %s'%(device_id, on_request, traceback.format_exc())
 
+
+def BeginMonitoringSonoffDevices(callbackfunc, dictDevices):
+    zeroconf = Zeroconf()
+    listener = sonoffListener(callbackfunc, dictDevices)
+    browser = ServiceBrowser(zeroconf, "_ewelink._tcp.local.", listener)
+    while True:  
+        time.sleep(600)
+
+class sonoffListener(ServiceListener):
+    import sonoffcrypto
+    callbackfunc = None
+    dictDevices = None
+    
+    def __init__(self, callbackfunc, dictDevices) -> None:
+        super().__init__()
+        self.callbackfunc = callbackfunc
+        self.dictDevices = dictDevices
+
+    def actionSonoffEvent(self, zc: Zeroconf, type_: str, name: str) -> None:
+        info = zc.get_service_info(type_, name)
+        device = info.properties[b"id"].decode("ascii")
+        data = info.properties.get(b"data1")
+        if info.properties.get(b"data2") is not None:
+            data += info.properties.get(b"data2")
+        if info.properties.get(b"data3") is not None:
+            data += info.properties.get(b"data3")
+        if info.properties.get(b"data4") is not None:
+            data += info.properties.get(b"data4")
+                                    
+        iv = info.properties.get(b"iv")
+        api_key = self.dictDevices[device][0]
+
+        data = self.sonoffcrypto.decrypt(data, iv, api_key)
+        data = json.loads(data.decode("ascii"))
+        
+        self.callbackfunc(device, data)
+        
+    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        self.actionSonoffEvent(zc, type_, name)
+
+    def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        self.actionSonoffEvent(zc, type_, name)
+
+    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        self.actionSonoffEvent(zc, type_, name)
 
 '''
-ip_address = '192.168.1.107'
-api_key = '8cc35b2c-d8c9-4eae-817a-68096a3e3e70'
-device_id = '100073bdf7' #not really required
+ip_address = '192.168.1.0'
+api_key = '54554454-d8c9-4eae-817a-45541245'
+device_id = '545512457' #not really required
 outlet=None #or integer if is strip socket
 err = change_switch(api_key, device_id, ip_address, outlet, str("ON").lower())
+
+
+to monitor just call:
+import sonoffsimpleLAN
+try:
+    sonoffsimpleLAN.BeginMonitoringSonoffDevices(SonoffCallback, dictNewSonoff)
+except:
+    pass
+    
+where SonoffCallback:
+
+def SonoffCallback(device, data):
+    try:
+        if 'switches' in data:
+            
+            for sw in data['switches']:
+                devstatus = 'ON' if sw['switch'] == 'on' else 'OFF'  
+                #sw['outlet'] gives the outlet
+                #do some stuff
+                            
+        else:
+            devstatus = 'ON' if data['switch'] == 'on' else 'OFF'        
+            strRSSI = 'rssi:%s'%data['rssi']
+            #do some stuff
+
+    except:
+        log_error(traceback.format_exc())
+
 '''
